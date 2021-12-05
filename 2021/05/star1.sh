734ab40bfa3c897b9965373132e0dac013f58698
ROBEzone <78911491+robezonetv@users.noreply.github.com>
2021-12-05 21:36:19 +0100

    finally

--- 2021/05/star1.sh
+++ 2021/05/star1.sh
@@ -14,14 +14,16 @@ while read line; do
 	x2=$(echo "$line" | cut -d"," -f2 | cut -d" " -f3)
 	y2=$(echo "$line" | cut -d"," -f3)
 
+	# DISABLE DIAGONAL BLOCK
 	if [[ $x1 -eq $x2 ]] || [[ $y1 -eq $y2 ]]; then
-		echo "PROCESS [$x1,$y1 -> $x2,$y2]"
+		echo "PROCESS [$x1,$y1 -> $x2,$y2]" &>/dev/null
 	else
 		continue
 	fi
+	# DISABLE DIAGONAL BLOCK
 
 	if [[ $x1 -gt $x2 ]]; then
-		#echo "FIRST"
+	#	echo "FIRST"
 		run=0
 		for i in $(seq $x2 $x1); do
 			if [[ $y1 -gt $y2 ]]; then
@@ -35,69 +37,55 @@ while read line; do
 			fi
 			run=$((run+1))
 		done
-	fi
-
-	if [[ $x1 -lt $x2 ]]; then
-		#echo "SECOND"
+	elif [[ $x1 -lt $x2 ]]; then
+	#	echo "SECOND"
 		run=0
                 for i in $(seq $x1 $x2); do
                         if [[ $y1 -gt $y2 ]]; then
-                                write "$i-$((y2+run))"
+                                write "$i-$((y1-run))"
                         fi
                         if [[ $y2 -gt $y1 ]]; then
-                                write "$i-$((y2-run))"
+                                write "$i-$((y1+run))"
                         fi
 			if [[ $y1 -eq $y2 ]]; then
 				write "$i-$y1"
 			fi
                         run=$((run+1))
                 done
-	fi
-
-	if [[ $x1 -eq $x2 ]]; then
-		if [[ $y1 -gt $y2 ]]; then
-			for i in $(seq $y2 $y1); do
-				write "$x1-$i"
-			done
-		fi
-		if [[ $y1 -lt $y2 ]]; then
-			for i in $(seq $y1 $y2); do
-				write "$x1-$i"
-			done
-		fi
-		if [[ $y1 -eq $y2 ]]; then
-			write "$x1-$y1"
-		fi
+        elif [[ $x1 -eq $x2 ]]; then
+	#	echo "THIRD"
+                if [[ $y1 -ge $y2 ]]; then
+                        for i in $(seq $y2 $y1); do
+                                write "$x1-$i"
+                        done
+                        continue
+                else
+                        for i in $(seq $y1 $y2); do
+                                write "$x1-$i"
+                        done
+                        continue
+                fi
+        elif [[ $y1 -eq $y2 ]]; then
+	#	echo "FOUR"
+                if [[ $x1 -ge $x2 ]]; then
+                        for i in $(seq $x2 $x1); do
+                                write "$i-$y1"
+                        done
+                        continue
+                else
+                        for i in $(seq $x1 $x2); do
+                                write "$i-$y1"
+                        done
+                        continue
+                fi
         fi
 
-	#echo "###"
+	#echo "NEXT"
 
 done <<<"$(cat ./input)"
 
 echo "PROCESSING DONE"
 
-#max=0i
-#imax_sum=0
-
 sum=$(ls -lah /tmp/board | grep -v "\." | grep -v "total" | tr -s " " | cut -d" " -f5,9| sort -k1 -n | grep -v "^2 " | wc -l)
-
-#for file in $(ls -1 ./board*); do
-#	count=$(wc -l ./board/$file | awk '{print$1}')
-#        if [[ $count -ge 2 ]]; then
-#	        sum=$((sum+1))
-#        fi
-#done
-
-#for j in $(seq 0 $board); do
-#	for i in $(seq 0 $board); do
-#		if [[ ! -e ./board/$i-$j ]]; then
-#			echo -ne "."
-#			continue
-#		fi
-#		count=$(wc -l ./board/$i-$j| awk '{print$1}')
-#		echo -ne "$count"
-#	done
-#	echo -ne "\n"
-#done
 
 echo "crossed lines count[$sum]"
