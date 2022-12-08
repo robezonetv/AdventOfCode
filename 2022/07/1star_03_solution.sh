fc4c09c2ede4a8ad0542d9d2784d768ead0783f2
robezonetv <78911491+robezonetv@users.noreply.github.com>
2022-12-07 18:53:29 -0600

    day 7 - I kill myself ... missing '/' solving for 4 hours!

--- 2022/07/1star_03_solution.sh
+++ 2022/07/1star_03_solution.sh
@@ -3,7 +3,7 @@
 final=0
 for i in $(find ./data -type d); do
     sum=0
-    for j in $(grep "$i" ./1star_all_size | awk '{print $2}'); do
+    for j in $(grep "$i/" ./1star_all_size | awk '{print $2}'); do
         sum=$(( sum + j ))
     done
 
